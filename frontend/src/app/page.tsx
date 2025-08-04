fcf4ab02b4fbf542373ba6240a045120eca948d7
Kit Jones <174150846+snarelord@users.noreply.github.com>
2025-08-04 18:36:43 +0100

    Create frontend skeleton

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,28 +1,17 @@
 import Image from "next/image";
 import Content from "@/components/Content";
+import styles from "./page.module.css";
 
 export default async function Home() {
-  // TODO: Fetch the data from /api/data
-
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <div className="flex flex-col justify-center items-center w-full">
-          <Image
-            src="/logo.png"
-            alt="Citizens Advice SORT"
-            width={150}
-            height={150}
-            priority
-          />
-          <h1 className="text-2xl font-bold mt-4">
-            Citizens Advice SORT
-          </h1>
-          <h2 className="text-lg font-medium">
-            Junior Developer Practical
-          </h2>
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <div className={styles.header}>
+          <Image src="/logo.png" alt="Citizens Advice SORT" width={150} height={150} priority />
+          <h1 className={styles.title}>Citizens Advice SORT</h1>
+          <h2 className={styles.subtitle}>Junior Developer Practical</h2>
         </div>
-        <Content /> {/* Each piece of content in the list should have its own content component */}
+        <Content />
       </main>
     </div>
   );
